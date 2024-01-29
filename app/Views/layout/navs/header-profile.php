be715938e86b49e5a269c3f1de9e29d662e5fae3
Rama Noor Rizki <ramanoorrizki@gmail.com>
2024-01-29 09:57:52 +0800

    Style: restyle header

--- app/Views/layout/navs/header-profile.php
+++ app/Views/layout/navs/header-profile.php
@@ -1,44 +1,53 @@
-<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur"
-  data-scroll="false">
-  <div class="container-fluid py-1 px-3 mt-3 ">
-    <nav aria-label="breadcrumb">
-      <span class="breadcrumb-item text-lg text-white active font-weight-bold">SISTEM INFORMASI DATA BIDANG</span>
-      <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-        <li class="breadcrumb-item text-sm">
-          <a class="opacity-5 text-white" href="javascript:;">Pages</a>
-        </li>
-        <li class="breadcrumb-item text-sm text-white active" aria-current="page">
-          Profile Bidang
-        </li>
-      </ol>
-      <h6 class="font-weight-bolder text-white mb-0">Profile Bidang</h6>
+<style>
+  /* CSS umum untuk semua ukuran layar */
+  .app-title {
+    font-size: 36px;
+    /* Ukuran teks default */
+  }
+
+  /* Media query untuk layar dengan lebar maksimal 767px (ukuran handphone) */
+  @media only screen and (max-width: 767px) {
+    .app-title {
+      font-size: 14px;
+      /* Ukuran teks untuk layar handphone */
+    }
+  }
+</style>
+
+
+<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" data-scroll="false">
+  <div class="container w-100 py-1 px-3 mt-3 ">
+    <nav aria-label="breadcrumb" class="w-100">
+      <div class="row w-100 d-flex">
+        <div class="col-10">
+          <div class="d-flex align-items-center">
+            <img style="width: 35px;height: 45px;" class="me-1" src="<?= base_url() ?>/assets/img/logo-pemko/logo-pemko.png" alt="">
+            <h1 class="app-title breadcrumb-item text-white active font-weight-bold">SISTEM INFORMASI DATA
+              BIDANG</h1>
+          </div>
+          <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+            <li class="breadcrumb-item text-sm">
+              <a class="opacity-5 text-white" href="javascript:;">Pages</a>
+            </li>
+            <li class="breadcrumb-item text-sm text-white active" aria-current="page">
+              Profile Bidang
+            </li>
+          </ol>
+          <h6 id="namaBidang" class="font-weight-bolder text-white mb-0"></h6>
+        </div>
+        <div class="col-2 d-flex justify-content-center align-items-center">
+          <img style="width: 75px; height: 75px;" src=" <?= base_url() ?>/assets/img/logo-pemko/logo-bjb-juara.png" alt="">
+          <li class="nav-item me-2 d-xl-none  d-flex align-items-center ">
+            <a href="javascript:;" class="nav-link text-white rounded-2 p-2 border border-2 border-light" id="iconNavbarSidenav">
+              <div class="sidenav-toggler-inner ">
+                <i class="sidenav-toggler-line bg-white"></i>
+                <i class="sidenav-toggler-line bg-white"></i>
+                <i class="sidenav-toggler-line bg-white"></i>
+              </div>
+            </a>
+          </li>
+        </div>
+      </div>
     </nav>
-    <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4 justify-content-end" id="navbar">
-
-      <!--
-            SEARCH BOX
-            <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-                <div class="input-group">
-                    <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
-                    <input type="text" class="form-control" placeholder="Type here..." />
-                </div>
-            </div>
-            -->
-
-      <ul class="navbar-nav justify-content-end">
-        <li class="nav-item me-2 d-xl-none ps-3 d-flex align-items-center ">
-          <a href="javascript:;" class="nav-link text-white rounded-2 p-2 border border-2 border-light"
-            id="iconNavbarSidenav">
-            <div class="sidenav-toggler-inner ">
-              <i class="sidenav-toggler-line bg-white"></i>
-              <i class="sidenav-toggler-line bg-white"></i>
-              <i class="sidenav-toggler-line bg-white"></i>
-            </div>
-          </a>
-        </li>
-
-
-      </ul>
-    </div>
   </div>
 </nav>
