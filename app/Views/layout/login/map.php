fa949d92586498b841df954fc6502a4b00b82b23
Rama Noor Rizki <ramanoorrizki@gmail.com>
2024-02-04 13:33:57 +0800

    Feat: add desc work hour on top of map in sign in page

--- app/Views/layout/login/map.php
+++ app/Views/layout/login/map.php
@@ -1,13 +1,21 @@
-<section id="about" >
+<section id="about">
+    <div class="container">
+        <div class="text-center">
+            <div class="col-md item text">
+                <h3 class="font-weight-bold">Jam Kerja</h3>
+                <div>
+                    <p class="m-0 p-0">Senin - Kamis 08.00-16.30</p>
+                    <p class="m-0 p-0">Jumat 08.00-11.00</p>
+                </div>
+            </div>
+        </div>
+    </div>
     <div style="padding-top: 100px; padding-bottom: 100px;" class="container">
         <div class="row">
             <div class="col-lg-12">
                 <div class="section-heading">
                     <div id="map">
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d497.82585218272516!2d114.832085!3d-3.445398!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2de681ab632b5d2f%3A0xed647956ee6993e6!2sKantor%20DP2KBPMP2A%20kota%20Banjarbaru!5e0!3m2!1sid!2sid!4v1703652883219!5m2!1sid!2sid"
-                            width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d497.82585218272516!2d114.832085!3d-3.445398!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2de681ab632b5d2f%3A0xed647956ee6993e6!2sKantor%20DP2KBPMP2A%20kota%20Banjarbaru!5e0!3m2!1sid!2sid!4v1703652883219!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
             </div>
