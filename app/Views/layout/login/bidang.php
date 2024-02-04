4293bfea19288df4ee440b5214665b102b97fa1d
Rama Noor Rizki <ramanoorrizki@gmail.com>
2024-02-04 11:21:15 +0800

    Style: change layout on bidang description in sign-in page

--- app/Views/layout/login/bidang.php
+++ app/Views/layout/login/bidang.php
@@ -1,11 +1,11 @@
 <section id="bidang" class="values">
-  <div class="container aos-init" data-aos="fade-up">
-    <header class="section-header">
-      <h2 style="font-size: 17pt">Bidang - Bidang</h2>
-    </header>
+    <div class="container aos-init" data-aos="fade-up">
+        <header class="section-header">
+            <h2 style="font-size: 17pt">Bidang - Bidang</h2>
+        </header>
 
-    <div id="list-bidang" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-      <!-- <div class="col mt-3">
+        <div id="list-bidang" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 justify-content-center">
+            <!-- <div class="col mt-3">
                 <div class="box aos-init" data-aos="fade-down" data-aos-delay="200">
                     <img src="<?= base_url() ?>/assets/img/DALDUK.gif" class="img-fluid" alt="" />
                     <h3 class="text-center">Sekretariat</h3>
@@ -70,6 +70,6 @@
                     </small>
                 </div>
             </div> -->
+        </div>
     </div>
-  </div>
 </section>
