ef9a51bb07622df3752d8009011fe3fdb5a633cd
Rama Noor Rizki <ramanoorrizki@gmail.com>
2024-02-04 11:32:46 +0800

    Style: change header nip to NIP/NRTK/NR on data pegawai page

--- app/Views/layout/tables/content-table-master-data-pegawai.php
+++ app/Views/layout/tables/content-table-master-data-pegawai.php
@@ -12,17 +12,19 @@
                       <h4 class="text-5xl font-weight-bolder text-center">Data Pegawai</h4>
                     </div>
                     <div>
-                      <button class="btn btn-success" role="button" data-bs-toggle="modal" data-bs-target="#exampleModalTambah5">Tambah</button>
+                      <button class="btn btn-success" role="button" data-bs-toggle="modal"
+                        data-bs-target="#exampleModalTambah5">Tambah</button>
                     </div>
                   </div>
                 </div>
                 <div class="card-body px-0 pt-0 pb-2">
                   <div class="table-responsive p-4">
-                    <table id="master_pegawaiDinas" style="width: 100%" class="table table-hover table-striped align-items-center mb-">
+                    <table id="master_pegawaiDinas" style="width: 100%"
+                      class="table table-hover table-striped align-items-center mb-">
                       <thead>
                         <tr>
                           <th width="%">Nama</th>
-                          <th width="20%">NIP</th>
+                          <th width="20%">NIP/NRTK/NR</th>
                           <th width="30%">Jabatan</th>
                           <th width="30%">Divisi</th>
                           <th width="20%">Aksi</th>
@@ -42,7 +44,8 @@
 
         <!-- modal simpan / tambah data pegawai dinas-->
         <div class="col-md-4">
-          <div class="modal fade" id="exampleModalTambah5" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal fade" id="exampleModalTambah5" tabindex="-1" role="dialog"
+            aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered " role="document">
               <div class="modal-content">
                 <div class="modal-body p-0">
@@ -63,7 +66,7 @@
                         </div>
                         <div class="mb-3">
                           <Label>
-                            NIP
+                            NIP/NRTK/NR
                           </Label>
                           <input type="text" name="nip" class="form-control" placeholder="NIP" aria-label="Nama">
                         </div>
@@ -71,7 +74,8 @@
                           <Label>
                             Jabatan
                           </Label>
-                          <input type="text" name="jabatan" class="form-control" placeholder="Jabatan" aria-label="Jabatan">
+                          <input type="text" name="jabatan" class="form-control" placeholder="Jabatan"
+                            aria-label="Jabatan">
                         </div>
                         <div class="mb-3">
                           <Label>
@@ -84,8 +88,10 @@
                     </div>
                     <div class="card-footer">
                       <div class="d-flex align-items-center justify-content-between">
-                        <button type="submit" class="btn bg-gradient-success w-25 my-4 mb-2" onclick="simpan_dataProfilePegawai()" data-bs-dismiss="modal">Buat</button>
-                        <button type="button" class="btn bg-gradient-warning w-25 my-4 mb-2" onclick="$('#simpan_ProfilePegawai')[0].reset();" data-bs-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn bg-gradient-success w-25 my-4 mb-2"
+                          onclick="simpan_dataProfilePegawai()" data-bs-dismiss="modal">Buat</button>
+                        <button type="button" class="btn bg-gradient-warning w-25 my-4 mb-2"
+                          onclick="$('#simpan_ProfilePegawai')[0].reset();" data-bs-dismiss="modal">Cancel</button>
                       </div>
                     </div>
                   </div>
@@ -97,7 +103,8 @@
 
         <!-- modal ubah data pegawai dinas-->
         <div class="col-md-4">
-          <div class="modal fade" id="modalUbahProfilePegawai" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal fade" id="modalUbahProfilePegawai" tabindex="-1" role="dialog"
+            aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered " role="document">
               <div class="modal-content">
                 <div class="modal-body p-0">
@@ -113,19 +120,22 @@
                           <Label>
                             Nama
                           </Label>
-                          <input type="text" name="nama" id="nama" class="form-control" placeholder="Nama" aria-label="Nama">
+                          <input type="text" name="nama" id="nama" class="form-control" placeholder="Nama"
+                            aria-label="Nama">
                         </div>
                         <div class="mb-3">
                           <Label>
-                            NIP
+                            NIP/NRTK/NR
                           </Label>
-                          <input type="text" name="nip" id="nip" class="form-control" placeholder="NIP" aria-label="Nama">
+                          <input type="text" name="nip" id="nip" class="form-control" placeholder="NIP"
+                            aria-label="Nama">
                         </div>
                         <div class="mb-3">
                           <Label>
                             Jabatan
                           </Label>
-                          <input type="text" name="jabatan" id="jabatan" class="form-control" placeholder="Jabatan" aria-label="Nama">
+                          <input type="text" name="jabatan" id="jabatan" class="form-control" placeholder="Jabatan"
+                            aria-label="Nama">
                         </div>
 
                         <div class="mb-3">
@@ -139,8 +149,10 @@
                     </div>
                     <div class="card-footer">
                       <div class="d-flex align-items-center justify-content-between">
-                        <button type="submit" class="btn bg-gradient-success w-25 my-4 mb-2" onclick="js_ubahProfilePegawai()" data-bs-dismiss="modal">Update</button>
-                        <button type="button" class="btn bg-gradient-warning w-25 my-4 mb-2" onclick="$('#ubah_ProfilePegawai')[0].reset();" data-bs-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn bg-gradient-success w-25 my-4 mb-2"
+                          onclick="js_ubahProfilePegawai()" data-bs-dismiss="modal">Update</button>
+                        <button type="button" class="btn bg-gradient-warning w-25 my-4 mb-2"
+                          onclick="$('#ubah_ProfilePegawai')[0].reset();" data-bs-dismiss="modal">Cancel</button>
                       </div>
                     </div>
                   </div>
